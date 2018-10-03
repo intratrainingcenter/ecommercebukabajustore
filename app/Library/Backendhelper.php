4b3f4a7205569bcc9e8b8c49b5d50ae00a0e6369
Jihan Ali Reza <myrathlatuconsina@gmail.com>
2018-10-03 20:05:21 +0700

    Check Helper Promo

--- app/Library/Backendhelper.php
+++ app/Library/Backendhelper.php
@@ -3,17 +3,6 @@ namespace App\Library;
 
 class Backendhelper
 {
-	public static function promo_read_update_delete_byid($id,$basemenu)
-	{
-		$data = '<a><button type="button" class="btn btn-outline-primary waves-effect waves-light pull-right deleteData" style="margin-right: 10px;" attr-id='.$id.' title="Hapus"><i class="fa fa-trash-o"></i>
-			</button></a>
-			<a href="'.$basemenu.'/edit'.$basemenu.'/'.$id.'"><button type="button" class="btn btn-outline-warning waves-effect waves-light pull-right" style="margin-right: 10px;" title="Edit"><i class="fa fa-pencil"></i>
-			</button></a>
-			<a href="'.$basemenu.'/detail'.$basemenu.'/'.$id.'"><button type="button" class="btn btn-outline-info waves-effect waves-light pull-right" style="margin-right: 10px;" title="Detail"><i class="fa fa-search"></i>
-			</button></a>';
-		return $data;
-	}
-
 	public static function read_update_delete_byid($id,$routeedit,$routedetail)
 	{
 		$data = '<a><button type="button" class="btn btn-outline-primary waves-effect waves-light pull-right deleteData" style="margin-right: 10px;" attr-id='.$id.' title="Hapus"><i class="fa fa-trash-o"></i>
