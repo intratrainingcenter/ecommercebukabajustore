377b677c973ed56b333d9df4546abfc2fb6c737c
ferdian666 <muhamadferdian539@gmail.com>
2018-10-06 16:31:34 +0700

    Checkpoint update about

--- resources/views/backend/about/tabledataabout.blade.php
+++ resources/views/backend/about/tabledataabout.blade.php
@@ -4,7 +4,6 @@
 @foreach($dataAbout as $itemAbout)
 <tr>
     <td>{{ $no++ }}</td>
-    <td>{{ $itemAbout->foto }}</td>
     <td>{{ $itemAbout->judul }}</td>
     <td> {{ $itemAbout->status }}</td>
     <td>{!!Backendhelper::read_update_delete_byid($itemAbout->id,route('aboutEdit',['id'=>$itemAbout->id]),route('aboutDetail',['id'=>$itemAbout->id]))!!}</td>
