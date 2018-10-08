4e08130a30ec22e8a1a91a365434571093f4d80d
ferdian666 <muhamadferdian539@gmail.com>
2018-10-08 09:37:59 +0700

    label for about

--- resources/views/backend/about/tabledataabout.blade.php
+++ resources/views/backend/about/tabledataabout.blade.php
@@ -5,7 +5,7 @@
 <tr>
     <td>{{ $no++ }}</td>
     <td>{{ $itemAbout->judul }}</td>
-    <td> {{ $itemAbout->status }}</td>
+    <td><i class=@if ($itemAbout->status == 'Active')"badge badge-success" @else "badge badge-primary" @endif >{{$itemAbout->status}}</i></td>
     <td>{!!Backendhelper::read_update_delete_byid($itemAbout->id,route('aboutEdit',['id'=>$itemAbout->id]),route('aboutDetail',['id'=>$itemAbout->id]))!!}</td>
 </tr>
 @endforeach
