476a9b9c3fc3ed2cce4feb326e5f7085886b8928
Jihan Ali Reza <myrathlatuconsina@gmail.com>
2018-10-09 21:15:19 +0700

    Generate Kode Transaksi

--- app/Http/Controllers/Frontend/FcartController.php
+++ app/Http/Controllers/Frontend/FcartController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Frontend;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-use App\Pemesanan as OrderModel;
+use App\Pemesanan as TransactionHistory;
+use App\Pemesanan_Temp as TransactionTemp;
 use App\Opsi_Pemesanan_Temp as Cart;
 
 
@@ -13,14 +14,37 @@ class FcartController extends Controller
 {
     public function addtocart(Request $request)
     {
-    	$codeTransaction = $this->generateCodeTransaction();
+        // Check table pemesanan apakah ada transaksi berstatus 'incart'
+        $incartTransactionTemp = TransactionTemp::where([['kode_user',1],['status','incart']])->get();
+        if($incartTransactionTemp->isEmpty()){
+            $codeTransaction = $this->generateCodeTransaction();
+        }else{
+            $codeTransaction = $incartTransactionTemp->kode_pemesanan;
+        }
     	return $codeTransaction;
     }
 
     public function generateCodeTransaction()
     {
-    	$checkTransaction = OrderModel::where('kode_user',1)->max('id');
+        $checkTransactionHistory = TransactionHistory::where('kode_user',1)->max('kode_pemesanan');
+    	$checkTransactionTemp = TransactionTemp::where('kode_user',1)->max('kode_pemesanan');
+
+        $codeMaxHistory = (!is_null($checkTransactionHistory))?$checkTransactionHistory:null;
+        $codeMaxTemp = (!is_null($checkTransactionTemp))?$checkTransactionTemp:null;
+
+        $sequenceHistory = (!is_null($codeMaxHistory))?substr($codeMaxHistory, 18):0; 
+        $sequenceTemp = (!is_null($codeMaxTemp))?substr($codeMaxTemp, 18):0; 
+
+        $sequence = ($sequenceTemp >= $sequenceHistory)?$sequenceTemp:$sequenceHistory;
+
+
+        $sequence+=1;   
+
+        $now=date("ymdhis");
+
+        // $codeTransaction = 'TR-'.$now.'U'.iduserauth.'M'.$sequence;
+        $codeTransaction = 'TR-'.$now.'U'.'1'.'M'.$sequence;
     	
-    	return $checkTransaction;
+    	return $codeTransaction;
     }
 }
---
+++ app/Pemesanan_Temp.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pemesanan_Temp extends Model
+{
+    protected $table = 'pemesanan_temps';
+}
