b60162260893542d2772c42deff18d8d6b4ff78b
var <ivaronosogrem315@gmail.com>
2018-11-01 17:01:51 +0700

    transaction & retur report
    show appear by status

--- app/Http/Controllers/Backend/BreporttransactionController.php
+++ app/Http/Controllers/Backend/BreporttransactionController.php
@@ -15,11 +15,11 @@ class BreporttransactionController extends Controller
     {
       $minutes = now()->addMinutes(1);
       $opsi_transaction = Cache::remember('reporttransaction',$minutes, function(){
-        return Opsi_Pemesanan::all();
+        return Opsi_Pemesanan::leftjoin('pemesanans', 'opsi_pemesanans.kode_pemesanan', '=', 'pemesanans.kode_pemesanan')->where('status','received')->get();
       });
 
-      $opsi_retur = Cache::remember('reporretur',$minutes, function(){
-        return Opsi_Retur::all();
+      $opsi_retur = Cache::remember('reportretur',$minutes, function(){
+        return Opsi_Retur::leftjoin('returs', 'opsi_returs.kode_retur', '=', 'returs.kode_retur')->where('status','received')->get();
       });
 
       $data = array(
@@ -38,39 +38,39 @@ class BreporttransactionController extends Controller
       // if select ( Transaction ) , date
       if ($req->category == 'Transaction' && $req->start_date && $req->end_date) {
         $data = Cache::remember('reporttransaction',$minutes, function(){
-          return Opsi_Pemesanan::all();
+          return Opsi_Pemesanan::leftjoin('pemesanans', 'opsi_pemesanans.kode_pemesanan', '=', 'pemesanans.kode_pemesanan')->where('status','received')->get();
         });
         $opsi_transaction = $data->where('updated_at','>=',$req->start_date)->where('updated_at','<=',$req->end_date);
       }
       // if select ( Transaction )
       elseif ($req->category == 'Transaction' && empty($req->start_date) && empty($req->end_date)) {
         $opsi_transaction = Cache::remember('reporttransaction',$minutes, function(){
-          return Opsi_Pemesanan::all();
+          return Opsi_Pemesanan::leftjoin('pemesanans', 'opsi_pemesanans.kode_pemesanan', '=', 'pemesanans.kode_pemesanan')->where('status','received')->get();
         });
       }
       // if select ( Retur ) , date
       elseif ($req->category == 'Retur' && $req->start_date && $req->end_date) {
         $data = Cache::remember('reporretur',$minutes, function(){
-          return Opsi_Retur::all();
+          return Opsi_Retur::leftjoin('returs', 'opsi_returs.kode_retur', '=', 'returs.kode_retur')->where('status','received')->get();
         });
         $opsi_retur = $data->where('updated_at','>=',$req->start_date)->where('updated_at','<=',$req->end_date);
       }
         // if select ( Retur )
       elseif ($req->category == 'Retur'  && empty($req->start_date) && empty($req->end_date)) {
         $opsi_retur = Cache::remember('reporretur',$minutes, function(){
-          return Opsi_Retur::all();
+          return Opsi_Retur::leftjoin('returs', 'opsi_returs.kode_retur', '=', 'returs.kode_retur')->where('status','received')->get();
         });
       }
       elseif ($req->category == 'all' && empty($req->start_date) && empty($req->end_date)) {
         $opsi_transaction = Cache::remember('reporttransaction',$minutes, function(){
-          return Opsi_Pemesanan::all();
+          return Opsi_Pemesanan::leftjoin('pemesanans', 'opsi_pemesanans.kode_pemesanan', '=', 'pemesanans.kode_pemesanan')->where('status','received')->get();
         });
         $opsi_retur = Cache::remember('reporretur',$minutes, function(){
-          return Opsi_Retur::all();
+          return Opsi_Retur::leftjoin('returs', 'opsi_returs.kode_retur', '=', 'returs.kode_retur')->where('status','received')->get();
         });
       }else {
         $data = Cache::remember('reporttransaction',$minutes, function(){
-          return Opsi_Pemesanan::all();
+          return Opsi_Pemesanan::leftjoin('pemesanans', 'opsi_pemesanans.kode_pemesanan', '=', 'pemesanans.kode_pemesanan')->where('status','received')->get();
         });
         $opsi_transaction = $data->where('updated_at','>=',$req->start_date)->where('updated_at','<=',$req->end_date);
       }
